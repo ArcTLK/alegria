46fb18fa2d54bc820922955b2e5fa69e42fd7d18
ArcTLK <adityanair313@gmail.com>
2019-12-23 20:32:32 +0530

    Saving to native storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Network } from '@ionic-native/network/ngx';
 import { CategoryService, loadCategoriesFactory } from './category.service';
 
 @NgModule({
@@ -27,11 +27,11 @@ import { CategoryService, loadCategoriesFactory } from './category.service';
     AppRoutingModule
   ],
   providers: [
-    StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     NativeStorage,
     GooglePlus,
+    Network,
     CategoryService,
     { provide: APP_INITIALIZER, useFactory: loadCategoriesFactory, deps: [CategoryService], multi: true }
   ],
