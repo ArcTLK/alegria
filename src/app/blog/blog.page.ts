85cb897134788f72c895de6bc619024cb947c4bf
ArcTLK <adityanair313@gmail.com>
2019-12-28 14:26:05 +0530

    Can view blogs

--- src/app/blog/blog.page.ts
+++ src/app/blog/blog.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.page.html',
   styleUrls: ['./blog.page.scss'],
 })
-export class BlogPage implements OnInit {
-
-  constructor() { }
+export class BlogPage implements OnInit, OnDestroy {
+  private paramMapSub: any;
+  public blog: any = {
+    title: 'Loading...',
+    content: 'Loading'
+  };
+  constructor(
+    private angularFirestore: AngularFirestore,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    // get blog id
+    this.paramMapSub = this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      // get category data from service
+      let blogSub = this.angularFirestore.doc('/blogs/' + paramMap.get('id')).snapshotChanges().subscribe(response => {
+        this.blog = response.payload.data();
+        // unsubscribe from changes
+        blogSub.unsubscribe();
+      });
+    });
+  }
+  ngOnDestroy() {
+    this.paramMapSub.unsubscribe();
   }
 
 }
