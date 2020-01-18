8113d6fa76b30531fda9727cc104132b3405251f
ArcTLK <adityanair313@gmail.com>
2020-01-18 16:49:57 +0530

    Disabled QR Scanner

--- src/app/qr-scanner/qr-scanner.page.ts
+++ src/app/qr-scanner/qr-scanner.page.ts
@@ -1,61 +1,76 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+/*
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
-import { ToastController, Platform, LoadingController } from '@ionic/angular';
-import { AngularFirestore } from '@angular/fire/firestore';
+*/
+import { /*ToastController, Platform, LoadingController, */AlertController } from '@ionic/angular';
+/*import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
+*/
 @Component({
   selector: 'app-qr-scanner',
   templateUrl: './qr-scanner.page.html',
   styleUrls: ['./qr-scanner.page.scss'],
 })
 export class QrScannerPage implements OnInit, OnDestroy {
-  private scanSub: any;
-  private userSub: any;
+  /*private scanSub: any;
+  private userSub: any;*/
   public userId: string = null;
   constructor(
-    private qrScanner: QRScanner,
+    /*private qrScanner: QRScanner,*/
     private router: Router,
-    private toastController: ToastController,
+    /*private toastController: ToastController,
     private platform: Platform,
     private angularFireAuth: AngularFireAuth,
     private angularFirestore: AngularFirestore,
     private androidPermissions: AndroidPermissions,
     private loadingController: LoadingController,
-    private inAppBrowser: InAppBrowser
+    private inAppBrowser: InAppBrowser,*/
+    private alertController: AlertController
   ) { }
 
   ionViewWillLeave() {
-    console.log(4);
-    window.document.querySelector('ion-app').classList.remove('cameraView');
-    this.qrScanner.pausePreview();
-    console.log(5);
+    //window.document.querySelector('ion-app').classList.remove('cameraView');
+    //this.qrScanner.pausePreview();
   }
   ionViewWillEnter() {
+    this.alertController.create({
+      header: 'Thank you',
+      message: 'We are happy with your participation in Alegria 2020\'s QR Code competition. Results will be announced soon!',
+      buttons: [
+        {
+          text: 'Go back',
+          handler: () => {
+            this.router.navigateByUrl('/home');
+          }
+        }
+      ]
+    }).then(alert => alert.present());
     // user subscription
-    console.log(1);
+    /*
     this.userSub = this.angularFireAuth.user.subscribe(user => {
       if (user !== null) {
         this.userId = user.uid;
-        console.log(2);
-        this.openScanner();
-        console.log(3);
+        //this.openScanner();
       }
-    });
+    });*/
+
   }
 
   ngOnDestroy() {
+    /*
     window.document.querySelector('ion-app').classList.remove('cameraView');
     this.scanSub.unsubscribe();
     this.qrScanner.destroy();
     this.userSub.unsubscribe();
     this.userId = null;
+    */
   }
   ngOnInit() {
   }
+  /*
   async startScan() {
     // start scanning
     this.scanSub = this.qrScanner.scan().subscribe(async (text: string) => {
@@ -116,5 +131,5 @@ export class QrScannerPage implements OnInit, OnDestroy {
       toast.present();
       this.router.navigateByUrl('/home');
     }
-  }
+  }*/
 }
